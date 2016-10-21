490286aea1d105c2b89b4a56071d4d32a826cc95
Acer <Acer@Acer-PC>
2016-10-21 03:24:42 +0300

    sysnc

---
+++ src/wlo1.java
@@ -0,0 +1,20 @@
+
+public class wlo1 {
+	public static void main (String[] arg){
+		display();
+		}//
+
+		private static void display() {
+			// TODO Auto-generated method stub
+			System.out.println("Hello my friends");
+			
+			////////
+			
+			System.out.println("Succeded1");
+			
+			
+			
+			
+			///
+		}
+}
